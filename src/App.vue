b711b248fd0b295c80add142e985e53eef851fe3
codwerk <louis_widdy@live.fr>
2020-06-17 23:23:14 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <add-blog></add-blog>
+  </div>
+</template>
+
+<script>
+import addBlog from './components/addBlog'
+
+export default {
+  name: 'App',
+  components: {
+    'add-blog': addBlog
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
